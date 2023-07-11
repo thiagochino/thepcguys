55c3abff7262fce2f8b8d4e21279020146272c66
thiagochino <thiagojl.01@gmail.com>
2023-07-10 21:17:29 -0300

    añadi el conexion php

--- php/conexion.php
+++ php/conexion.php
@@ -1,1 +1,18 @@
 <?php 
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "thepcguys";
+
+
+$conexion = new mysqli($servername, $username, $password, $dbname);
+
+if ($conexion->connect_error) {
+    die("Error de conexión a la base de datos: " . $conexion->connect_error);
+}
+
+ echo "Conexión exitosa a la base de datos.";
+
+ $conexion->close();
+?>
